8e30b2e7265f3fdbaa8de61f222ad2ee35791b01
Gaëlle Morin <morin.gaelle@gmail.com>
2023-09-17 22:39:13 +0200

    navbar: used Bootstrap's classes specificities to show-hide the navbar on desktop devices

--- src/scripts/lg-navbar-show-hide.js
+++ src/scripts/lg-navbar-show-hide.js
@@ -3,6 +3,6 @@ const menuButton = document.querySelector('#js-menu-button'),
     menuPanel = document.querySelector('#js-menu');
 
 menuButton.addEventListener('click', () => {
-    menuPanel.classList.contains('d-none') ?
-    menuPanel.classList.remove('d-none') : menuPanel.classList.add('d-none');
+    menuPanel.classList.contains('d-lg-block') ?
+    menuPanel.classList.remove('d-lg-block') : menuPanel.classList.add('d-lg-block');
 });
