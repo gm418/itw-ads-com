be16f003ccaa965a565ec4674eace43090ee7cd3
Gaëlle Morin <morin.gaelle@gmail.com>
2023-09-18 21:20:35 +0200

    fixbug: fixed bad page reload

--- src/scripts/search-show-hide.js
+++ src/scripts/search-show-hide.js
@@ -2,7 +2,8 @@
 const searchButton = document.querySelector('#js-search-button'),
     searchInput = document.querySelector('#js-search-input');
 
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener('click', (e) => {
     searchInput.classList.contains('d-none') ?
     searchInput.classList.remove('d-none') : searchInput.classList.add('d-none');
+    e.preventDefault();
 });
